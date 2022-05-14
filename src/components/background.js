737e39a72af4dbf091918b034aa54312fe2ff706
9rotama <aoim99.sc@gmail.com>
2022-05-14 12:17:29 +0900

    react18&r3fインストール

--- src/components/background.js
+++ src/components/background.js
@@ -2,8 +2,13 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 /** @jsx jsx */
 import { Global, css, jsx } from '@emotion/react';
+import { Canvas } from '@react-three/fiber'
+
 
 function Background(){
+  <div id="canvas-container">
+    <Canvas />
+  </div>
 }
 
 export default Background
