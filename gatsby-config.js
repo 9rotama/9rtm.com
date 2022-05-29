71bcee0464dc67f3437fca7a45a5d0edd68563aa
9rotama <aoim99.sc@gmail.com>
2022-05-30 00:24:58 +0900

    browser/config追加

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `9rtm.com`,
+    description: `portfolio`,
+    author: `@9rotama`,
+    siteUrl: `https://9rtm.com/`,
+  }
+  plugins: [],
+}
