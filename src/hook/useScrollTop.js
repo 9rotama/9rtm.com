acd90ff7482effb1b281090b5eb64b6f3ac0bea1
9rotama <aoim99.sc@gmail.com>
2022-05-31 18:24:12 +0900

    レスポンシブ

---
+++ src/hook/useScrollTop.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useScroll(){
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", setScrollPosition(window.pageYOffset));
+    setScrollPosition(window.pageYOffset);
+
+    return () => window.removeEventListener("scroll", setScrollPosition(window.pageYOffset));
+  }, []);
+
+  return scrollPosition;
+}
+
+export default useScroll;
