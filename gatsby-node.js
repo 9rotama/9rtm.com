01507c943093212e542d657c2fdb0cf6ca951073
9rotama <aoim99.sc@gmail.com>
2022-06-01 19:33:38 +0900

    netlifyの設定

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,3 @@
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions;
-  if (page.path === `/`) {
-      page.matchPath = `/*`;
-      createPage(page);
-  }
-};
-
 const resolve = require('path').resolve
 exports.onCreateWebpackConfig = ({ actions }) => {
 
