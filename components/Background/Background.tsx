b99b815ac02fa67aa622db1f58754a87ac4e7630
meg <65887771+9rotama@users.noreply.github.com>
2023-07-20 23:24:59 +0900

    背景にパターン追加

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,4 +1,4 @@
-import { useContext, Suspense } from "react";
+import { useContext, Suspense, useEffect } from "react";
 import { css } from "@emotion/react";
 import { Canvas } from "@react-three/fiber";
 import {
@@ -9,10 +9,12 @@ import {
 import ThreeText from "./ThreeText";
 import { backgroundStateContext } from "../_common/Layout";
 
+const transitionDur = 0.3;
+
 const Background = () => {
   const backgroundState = useContext(backgroundStateContext);
 
-  const canvasWrapStyle = css`
+  const kaomojiBackgroundWrapStyle = css`
     position: fixed;
     top: 0px;
     left: 0px;
@@ -21,34 +23,101 @@ const Background = () => {
     z-index: 0;
 
     opacity: ${backgroundState.switch ? "1" : "0"};
-    transition-duration: 0.3s;
+    transition-duration: ${transitionDur}s;
+  `;
+
+  const patternBackgroundStyle = css`
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    background-color: #d0dde9;
+    background-image: linear-gradient(
+        30deg,
+        #acc8e3 12%,
+        transparent 12.5%,
+        transparent 87%,
+        #acc8e3 87.5%,
+        #acc8e3
+      ),
+      linear-gradient(
+        150deg,
+        #acc8e3 12%,
+        transparent 12.5%,
+        transparent 87%,
+        #acc8e3 87.5%,
+        #acc8e3
+      ),
+      linear-gradient(
+        30deg,
+        #acc8e3 12%,
+        transparent 12.5%,
+        transparent 87%,
+        #acc8e3 87.5%,
+        #acc8e3
+      ),
+      linear-gradient(
+        150deg,
+        #acc8e3 12%,
+        transparent 12.5%,
+        transparent 87%,
+        #acc8e3 87.5%,
+        #acc8e3
+      ),
+      linear-gradient(
+        60deg,
+        #acc8e377 25%,
+        transparent 25.5%,
+        transparent 75%,
+        #acc8e377 75%,
+        #acc8e377
+      ),
+      linear-gradient(
+        60deg,
+        #acc8e377 25%,
+        transparent 25.5%,
+        transparent 75%,
+        #acc8e377 75%,
+        #acc8e377
+      );
+    background-size: 74px 130px;
+    background-position: 0 0, 0 0, 37px 65px, 37px 65px, 0 0, 37px 65px;
+    animation: tx 1s linear infinite;
+    animation: gradient 15s linear infinite;
+    opacity: ${backgroundState.switch ? "0.4" : "0.6"};
+
+    transition-duration: ${transitionDur}s;
   `;
 
   return (
-    <div css={canvasWrapStyle}>
+    <>
+      <div css={patternBackgroundStyle} />
       {backgroundState.render ? (
         //ローディング中は表示がおかしくなるので何も表示しない
-        <Suspense fallback={<></>}>
-          <Canvas
-            gl={{ antialias: false }}
-            camera={{
-              fov: 45,
-              near: 0.1,
-              far: 1000,
-              position: [-60, 0, 0],
-            }}
-          >
-            <ThreeText />
-            <EffectComposer>
-              <ToneMapping />
-              <DotScreen angle={Math.PI * 0.5} scale={0.3} />
-            </EffectComposer>
-          </Canvas>
-        </Suspense>
+        <div css={kaomojiBackgroundWrapStyle}>
+          <Suspense fallback={<></>}>
+            <Canvas
+              gl={{ antialias: false }}
+              camera={{
+                fov: 45,
+                near: 0.1,
+                far: 1000,
+                position: [-60, 0, 0],
+              }}
+            >
+              <ThreeText />
+              <EffectComposer>
+                <ToneMapping />
+                <DotScreen angle={Math.PI * 0.5} scale={0.3} />
+              </EffectComposer>
+            </Canvas>
+          </Suspense>
+        </div>
       ) : (
         <></>
       )}
-    </div>
+    </>
   );
 };
 
