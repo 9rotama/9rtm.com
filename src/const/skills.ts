cccf2e79d128cd24baebec56571431d9a8a8dd9d
9rotama <65887771+9rotama@users.noreply.github.com>
2024-01-07 00:08:14 +0900

    内容更新

--- src/const/skills.ts
+++ src/const/skills.ts
@@ -1,5 +1,6 @@
 export const skills = [
   { name: "Next.js", iconSrc: "/tech-logo/next-js.svg" },
+  { name: "SvelteKit", iconSrc: "/tech-logo/svelte.png" },
   { name: "Python", iconSrc: "/tech-logo/python.webp" },
   { name: "TypeScript", iconSrc: "/tech-logo/ts.png" },
   { name: "Unity", iconSrc: "/tech-logo/unity.png" },
